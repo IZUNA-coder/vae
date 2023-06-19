706771119f010461ce6fa1304ecd258bd3a48620
DEBRAY Antoine <o22202147@info26-06.iut45.univ-orleans.fr>
2023-06-19 10:31:28 +0200

    ajout categorie

---
+++ Code/src/Modele/Categorie.java
@@ -0,0 +1,21 @@
+public class Categorie {
+    private int identifiant;
+    private String nom;
+    
+    public Categorie(int identifiant, String nom) {
+        this.identifiant = identifiant;
+        this.nom = nom;
+    }
+    public int getIdentifiant() {
+        return identifiant;
+    }
+    public void setIdentifiant(int identifiant) {
+        this.identifiant = identifiant;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
