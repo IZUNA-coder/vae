cb7e23f667efbded2c0d1fdcb8ed2fe30d66c765
SOTTIER Liam <o22202962@info26-09.iut45.univ-orleans.fr>
2023-06-19 09:39:29 +0200

    debut enchere

---
+++ Code/src/Modele/Enchere.java
@@ -0,0 +1,46 @@
+
+/**
+ * Enchere
+ */
+public class Enchere {
+
+    private int identifiant;
+    private Double prix;
+    private Utilisateur utilisateur;
+    private Vente vente;
+    private Objet objet;
+
+    public int getIdentifiant() {
+        return identifiant;
+    }
+
+    public Double getPrix() {
+        return prix;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public Vente getVente() {
+        return vente;
+    }
+
+    public Objet getObjet() {
+        return objet;
+    }
+
+    public Enchere(int identifiant, Double prix, Utilisateur utilisateur, Vente vente, Objet objet) {
+        this.identifiant = identifiant;
+        this.prix = prix;
+        this.utilisateur = utilisateur;
+        this.vente = vente;
+        this.objet = objet;
+    }
+
+    
+
+    
+
+    
+}
